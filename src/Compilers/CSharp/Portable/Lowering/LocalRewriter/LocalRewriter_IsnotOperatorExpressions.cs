645372412aa3f02adc89bbd6fd0e20bc9bce8ce1
Eik Rentzow <rentzow@gmx.de>
2017-05-14 19:17:46 +0200

    Changing operator bindings

---
+++ src/Compilers/CSharp/Portable/Lowering/LocalRewriter/LocalRewriter_IsnotOperatorExpressions.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System.Diagnostics;
+using Microsoft.CodeAnalysis.CSharp.Symbols;
+using Roslyn.Utilities;
+using System.Collections.Immutable;
+using System.Collections.Generic;
+
+namespace Microsoft.CodeAnalysis.CSharp
+{
+    internal sealed partial class LocalRewriter
+    {
+        public override BoundNode VisitIsnotOperator(BoundIsnotOperator node)
+        {
+            var boolean = GetSpecialType(SpecialType.System_Boolean, diagnostics, node);
+            var boundIsOperator = new BoundIsOperator(node, node.Operand, isOperator.TargetType, isOperator.Conversion, isOperator.Type);
+
+            return new BoundUnaryOperator(node, UnaryOperatorKind.BoolLogicalNegation, boundOperator,
+                ConstantValue.NotAvailable, null, LookupResultKind.Viable, boolean)
+                { WasCompilerGenerated = false };
+        }
+
+    }
+}
